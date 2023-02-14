0dad3328161fcd3b5eb574f0dad7ed5464b0c092
Devan Rivera <devanrivera98@gmail.com>
2023-02-14 05:56:58 +0000

    Add a 'submit' event listener to the form element.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-var $input = document.querySelector('input[name=photo');
+var $photoUrl = document.querySelector('input[name=photo]');
 var $img = document.querySelector('img');
 
-$input.addEventListener('input', setImage);
+$photoUrl.addEventListener('input', setImage);
 
 function setImage(event) {
-  $img.src = $input.value;
+  $img.src = $photoUrl.value;
+}
+
+// console.log(data.nextEntryId);
+// console.log(data.entries);
+var $form = document.querySelector('form');
+// console.log($form);
+var $title = document.querySelector('input[name=title]');
+var $notes = document.querySelector('textarea');
+// individually query select all three
+$form.addEventListener('submit', setSubmit);
+
+function setSubmit(event) {
+  event.preventDefault();
+  var object = {
+    title: $title.value,
+    url: $photoUrl.value,
+    notes: $notes.value,
+    entryId: 1
+  };
+  data.nextEntryId++;
+  // console.log(data.nextEntryId);
+  data.entries.unshift(object);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
