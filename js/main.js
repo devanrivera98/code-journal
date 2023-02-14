ff6274c8a536fad7b9c6a213df3835e1961197a2
Devan Rivera <devanrivera98@gmail.com>
2023-02-14 23:05:45 +0000

    The function should generate and return a DOM tree for a single entry that matches the entries you created in the unordered list.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,41 @@ function setSubmit(event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+function renderEntry(entry) {
+  var $divOne = document.createElement('div');
+  $divOne.setAttribute('class', 'row');
+
+  var $divTwo = document.createElement('div');
+  $divTwo.setAttribute('class', 'row column-half');
+
+  var $img = document.createElement('img');
+  $img.setAttribute('class', data.entries.url);
+
+  var $divThree = document.createElement('div');
+  $divThree.setAttribute('class', 'column-half');
+
+  var $ul = document.createElement('ul');
+
+  var $liOne = document.createElement('li');
+  $liOne.setAttribute('class', 'li-h1');
+
+  var $h2 = document.createElement('h2');
+  $h2.textContent = data.entries.title;
+
+  var $liTwo = document.createElement('li');
+  $liTwo.setAttribute('class', 'li-text');
+  $liTwo.textContent = data.entries.notes;
+
+  $divOne.appendChild($divTwo);
+  $divTwo.appendChild($img);
+  $divOne.appendChild($divThree);
+  $divThree.appendChild($ul);
+  $ul.appendChild($liOne);
+  $liOne.appendChild($h2);
+  $ul.appendChild($liTwo);
+
+  return $divOne;
+}
+renderEntry();
+// entry parameter represents a single object for a single entry from the data.entries array.
